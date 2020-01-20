f1a031e3b8a8bea779355e9975f2248985135250
mohsen <mansourvar.mohsen@gmail.com>
2020-01-20 11:10:25 +0300

    23-Add a controller for loading all students

---
+++ src/main/java/com/education/controller/StudentController.java
@@ -0,0 +1,19 @@
+package com.education.controller;
+
+import com.education.domain.Student;
+import com.education.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping(value = "/student/{id}")
+    public Student getById(@PathVariable long id) {
+        return studentService.getById(id);
+    }
+}
