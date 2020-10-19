60393dec70c94604a80d15262944bb0c623da599
mohsen <mansourvar.mohsen@gmail.com>
2020-10-19 22:34:49 +0330

    159-Apply Lombok annotations JUST to the Class entity

--- src/main/java/com/education/domain/Class.java
+++ src/main/java/com/education/domain/Class.java
@@ -2,45 +2,23 @@ package com.education.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Data
 @Entity
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
+
 public class Class {
     @Id
     @GeneratedValue
     private Long id;
     private String classNumber;
     private int capacity;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getClassNumber() {
-        return classNumber;
-    }
-
-    public void setClassNumber(String roomNumber) {
-        this.classNumber = roomNumber;
-    }
-
-    public int getCapacity() {
-        return capacity;
-    }
-
-    public void setCapacity(int capacity) {
-        this.capacity = capacity;
-    }
-
 }
