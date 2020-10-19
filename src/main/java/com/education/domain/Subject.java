6d7f89ead63538d1e9cf726c42e0dc59533cc0ed
mohsen <mansourvar.mohsen@gmail.com>
2020-10-19 22:42:57 +0330

    161-Apply Lombok annotations JUST to the Subject entity

--- src/main/java/com/education/domain/Subject.java
+++ src/main/java/com/education/domain/Subject.java
@@ -1,10 +1,20 @@
 package com.education.domain;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
+@Data
 @Entity
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class Subject {
 
     @Id
@@ -12,28 +22,4 @@ public class Subject {
     private Long id;
     private String name;
     private int unitNumber;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public int getUnitNumber() {
-        return unitNumber;
-    }
-
-    public void setUnitNumber(int unitNumber) {
-        this.unitNumber = unitNumber;
-    }
 }
